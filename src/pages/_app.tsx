eeec219cae960f7706b6de9642cdad020255328a
Daniel Kuiper <daniel@ospr.nl>
2022-01-10 15:42:37 +0100

    Fix trpc url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import '../styles/globals.css'
+import { withTRPC } from '@trpc/next';
 import type { AppProps } from 'next/app'
+import type { AppRouter } from "@/backend/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-import { withTRPC } from '@trpc/next';
-import type { AppRouter } from "@/backend/router";
+function getBaseURL(): String {
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
@@ -15,9 +20,7 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = `${getBaseURL()}/api/trcp`;
 
     return {
       url,
